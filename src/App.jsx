e60791d654f6b9ff29098dc4135b9f966170f11a
Emily Yang <emilyyang@Emilys-MacBook-Pro.local>
2025-01-15 21:43:15 +0100

    feat:iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import { useState } from "react";
+import FoodBox from "./components/FoodBox"
+
 
 function App() {
+  const [food, setFood] = useState(foodsJson);
+   
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      {food.map ( (el) => <FoodBox food = {el} />)}
     </div>
   );
 }
